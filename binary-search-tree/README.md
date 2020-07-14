54f2e56d285bfeac13c907a312aedc9b5bafee3a
Taylor Brazelton <taylor.r.brazelton@gmail.com>
2020-07-14 17:36:23 -0400

    Initial Binary Search Tree.

---
+++ binary-search-tree/README.md
@@ -0,0 +1,20 @@
+# Binary Search Tree
+
+A BST consist of a root note with two branches, one on the left that is made up of nodes with lesser value, and one on the right that is made up of nodes with greater value. An example of a binary search tree is an Abstract Syntax Tree. These are used a lot with static code analysis tools. BST is also the basis for a lot of other tree like data structures such as red-black and AVL trees.
+
+Here is an example from Wikipedia:
+
+![Image of a BST](https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/Binary_search_tree.svg/200px-Binary_search_tree.svg.png)
+
+
+Supported Operations:
+ - Insert
+ - Delete
+ - Search
+ - Pre-Order Traversal (node -> left subtree -> right subtree)
+ - In-Order Traversal (left subtree -> node -> right subtree)
+ - Post-Order Traversal (left subtree -> right subtree -> node)
+
+Types of BST:
+
+Unbalanced Tree.
---
+++ binary-search-tree/main.py
@@ -0,0 +1,41 @@
+
+
+class Node(object):
+    """
+    A single node of a BST.
+    """
+
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+    def is_leaf(self):
+        """Check to see if this is a left node."""
+        if not self.left and not self.right:
+            return True
+        return False
+
+    def print_tree(self, level=0):
+        preceding_space = "  " * level
+        print(f"{preceding_space}{self.value}")
+        if self.left:
+            self.left.print_tree(level=level+1)
+        if self.right:
+            self.right.print_tree(level=level+1)
+
+
+# Build up an example tree:
+
+root_node = Node(10)
+root_node.left = Node(7)
+root_node.left.left = Node(6)
+root_node.left.right = Node(8)
+
+root_node.right = Node(12)
+root_node.right.left = Node(11)
+root_node.right.right = Node(15)
+
+# Print out tree using breadth-first search allowing each layer
+# to be printed at a time.
+root_node.print_tree()
